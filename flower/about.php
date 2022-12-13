cadfd9d055b930650d401471a2f2c84a21bc72dc
abhishekshuklaaaa <112251630+abhishekshuklaaaa@users.noreply.github.com>
2022-12-13 23:14:34 +0530

    Add files via upload

---
+++ flower/about.php
@@ -0,0 +1,188 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>about us</h3>
+    <p> <a href="home.php">home</a> / about </p>
+</section>
+
+<section class="about">
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="/images/about-img-1.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>100% Satisfaction is our goal, we make sure you that you will be satisfied by our service</p>
+            <a href="shop.php" class="btn">shop now</a>
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="content">
+            <h3>what we provide?</h3>
+            <p>100% organic, farm-to-table flowers , You bring the love. We’ll bring the flowers.</p>
+            <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+        <div class="image">
+            <img src="images/about-img-2.jpg" alt="">
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>who we are?</h3>
+            <p>A florist completes an assortment of things in a retail bloom shop. Our primary concern is to provide best service.</p>
+            <a href="#reviews" class="btn">clients reviews</a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="reviews" id="reviews">
+
+    <h1 class="title">client's reviews</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic-1.png" alt="">
+            <p>A fantastic organisation! Great cutomer support from beginning to end of the process. The team are really informed and go the extra mile at every stage. I would recommend them unreservedly.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-2.png" alt="">
+            <p>Great customer service. Even when there was a problem with paperwork the employees continued helping us and fixed the issue. Very patient workers </p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-3.png" alt="">
+            <p>A fantastic organisation! Great cutomer support from beginning to end of the process. The team are really informed and go the extra mile at every stage. I would recommend them unreservedly</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-4.png" alt="">
+            <p>Great customer service. Even when there was a problem with paperwork the employees continued helping us and fixed the issue. Very patient workers</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-5.png" alt="">
+            <p>Great customer service. Even when there was a problem with paperwork the employees continued helping us and fixed the issue. Very patient workers</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-6.png" alt="">
+            <p>A fantastic organisation! Great cutomer support from beginning to end of the process. The team are really informed and go the extra mile at every stage. I would recommend them unreservedly</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ flower/footer.php
@@ -0,0 +1,37 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+             <p> <i class="fas fa-envelope"></i> avi07102002@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i>Kanpur, India  </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+           
+            <a href="www.linkedin.com/in/abhishek-shukla7"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Abhishek Shukla</span> </div>
+
+</section>
